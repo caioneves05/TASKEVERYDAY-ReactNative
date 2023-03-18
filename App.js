73a20fa9266716af1f715e24b1576bfcf181c051
Caio Neves <ncaio037@gmail.com>
2023-03-18 16:54:41 -0300

    Adicionando a tela home do aplicativo.

---
+++ App.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function Home() {
+  const [userType, setUserType] = useState('');
+
+  const handleUserTypeChange = (type) => {
+    setUserType(type);
+  }
+
+  const handleEnterAppPress = () => {
+    // Implemente aqui a navegação para a próxima tela do aplicativo
+    alert(`Bem-vindo, ${userType}!`);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao Task Every Day!</Text>
+      <Text style={styles.subtitle}>Você é professor ou aluno?</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, userType === 'professor' && styles.selectedButton]}
+          onPress={() => handleUserTypeChange('professor')}
+        >
+          <Text style={[styles.buttonText, userType === 'professor' && styles.selectedButtonText]}>Professor</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, userType === 'aluno' && styles.selectedButton]}
+          onPress={() => handleUserTypeChange('aluno')}
+        >
+          <Text style={[styles.buttonText, userType === 'aluno' && styles.selectedButtonText]}>Aluno</Text>
+        </TouchableOpacity>
+      </View>
+      {userType ? (
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.enterButton}
+            onPress={handleEnterAppPress}
+          >
+            <Text style={styles.enterButtonText}>Entrar no aplicativo</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3A035C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#fafafa'
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#fafafa'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    gap: 10
+  },
+  button: {
+    backgroundColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: '30%',
+    alignItems: 'center',
+    gap: 10
+  },
+  selectedButton: {
+    backgroundColor: '#9826DB',
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+  selectedButtonText: {
+    color: '#fff',
+  },
+  enterButton: {
+    backgroundColor: '#1A001F',
+    borderRadius: 5,
+    padding: 15,
+    borderRadius: 15,
+    width: '80%',
+    alignItems: 'center',
+    marginTop: 15
+  },
+  enterButtonText: {
+    fontSize: 16,
+    color: '#fff',
+  },
+});
---
+++ tasks.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import { Button, Input, ListItem, Icon } from 'react-native-elements';
+import Modal from 'react-native-modal';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+
+const Task = ({ task, onDelete, onView }) => {
+    const handlePress = () => {
+      onView(task);
+    };
+  
+    return (
+      <ListItem bottomDivider onPress={handlePress}  style={styles.taskContainer}>
+        <Icon name='delete' onPress={() => onDelete(task.id)} />
+        <ListItem.Content style={styles.taskBody}>
+          <ListItem.Title>{task.title}</ListItem.Title>
+          <ListItem.Subtitle>{task.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+};
+  
+export default function Tasks() {
+    const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selectedTask, setSelectedTask] = useState(null);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+  
+    const handleAddTask = () => {
+      if(title.trim() !== '' || description.trim() !== ''){
+        const newTask = {
+          id: Math.random(),
+          title,
+          description,
+        };
+          setTasks([...tasks, newTask]);
+          setTitle('');
+          setDescription('');
+      };
+  
+    }
+  
+    const handleDeleteTask = (id) => {
+      const newTasks = tasks.filter((task) => task.id !== id);
+      setTasks(newTasks);
+    };
+  
+    const handleViewTask = (task) => {
+      setSelectedTask(task);
+      setIsModalVisible(true);
+    };
+  
+    const closeModal = () => {
+      setSelectedTask(null);
+      setIsModalVisible(false);
+    };
+  
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <Input
+            label='Título'
+            value={title}
+            onChangeText={setTitle}
+            containerStyle={styles.inputContainer}
+            inputStyle={styles.input}
+            inputContainerStyle={styles.inputContainerStyle}
+          />
+          <Input
+            label='Descrição'
+            value={description}
+            onChangeText={setDescription}
+            containerStyle={styles.inputContainer}
+            inputStyle={styles.input}
+            inputContainerStyle={styles.inputContainerStyle}
+          />
+          <Button
+            title='Add Task'
+            onPress={handleAddTask}
+            buttonStyle={styles.addButton}
+          />
+          <View>
+            {tasks.map((task) => (
+              <Task key={task.id} task={task} onDelete={handleDeleteTask} onView={handleViewTask} />
+            ))}
+          </View>
+        </ScrollView>
+        <Modal isVisible={isModalVisible} onBackdropPress={closeModal} style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>{selectedTask?.title}</Text>
+            <Text style={styles.modalDescription}>{selectedTask?.description}</Text>
+          </View>
+          <Button title='Close' onPress={closeModal} buttonStyle={styles.modalCloseButton} />
+        </Modal>
+      </SafeAreaView>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      width: 300,
+      marginBottom: 16,
+      backgroundColor: '#fafafa',
+      padding: 18,
+      borderRadius: 20,
+  
+    },
+    input: {
+      backgroundColor: '#D9D9D9',
+      borderRadius: 10,
+      marginTop: 10
+    },
+    addButton: {
+      backgroundColor: '#9C00E6',
+      borderRadius: 14
+    },
+    taskContainer: {
+      marginTop: 20,
+    },
+    taskBody: {
+      backgroundColor: '#D9D9D9',
+      padding: 18
+    },
+    modalContainer: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    modalContent: {
+      width:300,
+      height: 100,
+      backgroundColor:'#D9D9D9',
+      borderRadius: 18,
+    },
+    modalTitle: {
+      textAlign: 'center',
+      fontSize: 15,
+      marginTop: 10,
+    },
+    modalDescription: {
+      textAlign: 'center',
+      fontSize: 12,
+      marginTop: 10,
+    },
+    modalCloseButton: {
+      width:300,
+      borderRadius: 18,
+      marginTop: 10,
+      backgroundColor: '#210F38',
+    }
+  })
